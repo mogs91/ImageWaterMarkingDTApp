d63f929b919f05f418d6a03dee4dd5237660dd4e
Mogs91 <markossayan@gmail.com>
2023-01-08 19:38:19 +0100

    setting up pillow and tkinter adding the sample.jpg image and the images dir

--- main.py
+++ main.py
@@ -1,16 +1,42 @@
-# This is a sample Python script.
+#imports
+
+from tkinter import *
+
+from PIL import Image
+
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def desktop_app():
+    # Basic instantiation
+
+    ##TKinter
+    window = Tk()
+    window.title("Image WaterMarking")
+    window.minsize(width=800, height=800)
+    # Adding padding
+    window.config(padx=40, pady=40)
+
+    ##Pillow
+
+    #Todo: We need to add two buttons on the bottom of the window
+    #We can use a stack layout image on top and two buttons on the bottom
+    #Todo:one button for adding the image - here we should use Pillow
+    #Todo:one button for applying the watermark
+
+
+    window.mainloop()
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('Markus')
+    desktop_app()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+
+#Todo: We will use Tkinter to create a  GUI that takes a image
+#TODO: You can then click a button "WaterMark Image" to add a logo/text to the image
+#Todo:  we might need to use Pillow https://pypi.org/project/Pillow/
+
