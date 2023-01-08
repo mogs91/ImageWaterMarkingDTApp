3e17356fdae5a4a5dc7f19de02efbf22d89da11e
Mogs91 <markossayan@gmail.com>
2023-01-08 20:48:26 +0100

    need to add the watermarking function

--- main.py
+++ main.py
@@ -1,55 +1,46 @@
-#imports
+##imports
 from tkinter import *
 
-# loading Python Imaging Library
 from PIL import ImageTk, Image
 
-# To get the dialog box to open when required
 from tkinter import filedialog
+
 
 def desktop_app():
     # Basic instantiation
 
     def openfilename():
-        # open file dialog box to select image
-        # The dialogue box has a title "Open"
-        filename = filedialog.askopenfilename(title='"pen')
+        """it opens a file dialog box to select image,
+        The dialogue box has a title 'Select image' """
+        filename = filedialog.askopenfilename(title='Select image')
         return filename
 
     def open_img():
-        # Select the Imagename  from a folder
-        x = openfilename()
-
-        # opens the image
-        img = Image.open(x)
-
-        # resize the image and apply a high-quality down sampling filter
-        img = img.resize((450, 450), Image.ANTIALIAS)
-
-        # PhotoImage class is used to add image to widgets, icons etc
+        """Select the Image name  from a folder,
+        ,opens the image,resize the image and apply a high-quality down sampling filter
+        ,PhotoImage class is used to add image to widgets, icons etc
+        ,create a label  # set the image as img"""
+        image = openfilename()
+        img = Image.open(image)
+        img = img.resize((450, 450))
         img = ImageTk.PhotoImage(img)
-
-        # create a label
         panel = Label(window, image=img)
-
-        # set the image as img
         panel.image = img
         panel.grid(row=2)
 
-    ##TKinter
+    ##TKinter Setup
     window = Tk()
     window.title("Image WaterMarking")
     window.minsize(width=800, height=800)
-    # Allow Window to be resizable
     window.resizable(width=True, height=True)
-    # Adding padding
     window.config(padx=40, pady=40)
 
-
-
-    # Create a button and place it into the window using grid layout
-    upload_image_btn = Button(window, text='upload image', command=open_img).grid(
+    upload_image_btn = Button(text='upload image', command=open_img).grid(
         row=1, columnspan=4)
+
+#Todo: need to add the watermarking functionality
+    watermark_btn = Button(text='watermark').grid(
+        row=4, columnspan=4)
 
 
 
@@ -59,10 +50,4 @@ def desktop_app():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     desktop_app()
-
-
-
-#Todo: We will use Tkinter to create a  GUI that takes a image
-#TODO: You can then click a button "WaterMark Image" to add a logo/text to the image
-#Todo:  we might need to use Pillow https://pypi.org/project/Pillow/
 
