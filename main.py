ff5bb9a892da737d08db2cfeec33719fa7277823
Mogs91 <markossayan@gmail.com>
2023-01-09 18:35:04 +0100

    Watermark function added and working

--- main.py
+++ main.py
@@ -1,13 +1,32 @@
 ##imports
 from tkinter import *
 
-from PIL import ImageTk, Image
+from PIL import ImageTk, Image,ImageDraw, ImageFont
 
 from tkinter import filedialog
 
 
 def desktop_app():
     # Basic instantiation
+    def watermark():
+        # get an image
+        image=openfilename()
+        with Image.open(image).convert("RGBA") as base:
+            # make a blank image for the text, initialized to transparent text color
+            txt = Image.new("RGBA", base.size, (255, 255, 255, 0))
+
+            # get a drawing context
+            d = ImageDraw.Draw(txt)
+
+            # draw text, half opacity
+            d.text((10, 10), "Hello", fill=(255, 255, 255, 128))
+            # draw text, full opacity
+            d.text((10, 60), "World",  fill=(255, 255, 255, 255))
+
+            out = Image.alpha_composite(base, txt)
+
+            out.show()
+            out.save('filename_out.png')
 
     def openfilename():
         """it opens a file dialog box to select image,
@@ -39,7 +58,7 @@ def desktop_app():
         row=1, columnspan=4)
 
 #Todo: need to add the watermarking functionality
-    watermark_btn = Button(text='watermark').grid(
+    watermark_btn = Button(text='watermark', command=watermark).grid(
         row=4, columnspan=4)
 
 
